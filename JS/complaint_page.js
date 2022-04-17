beaca05487f8114cf827f9d0a2ff3aa773b0b7d8
khushboo-rathod <co2019.khushboo.rathod@ves.ac.in>
2022-04-17 16:46:20 +0530

    ok

--- JS/complaint_page.js
+++
@@ -1,7 +0,0 @@
-function takeInput(){
-    let input = document.createElement('input');
-    input.type = 'file';
-    input.accept = 'image/*'
-    let file = input.file;
-    input.click();
-}
--- JS/homepage.js
+++ JS/homepage.js
@@ -16,3 +16,10 @@ function checkField(){
         location.href='/HTML/API_Page.html'
     }
 }
+function takeInput(){
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*'
+    let file = input.file;
+    input.click();
+}
