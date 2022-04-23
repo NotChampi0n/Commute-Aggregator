3e72c88079c89cee1647e3e7afe6d800cc3a05f8
NotChampi0n <co2019.ashwin.keshav@ves.ac.in>
2022-04-23 18:41:05 +0530

    API Sliders Fixed

--- JS/api_responsive.js
+++ JS/api_responsive.js
@@ -12,9 +12,11 @@ document.addEventListener("DOMContentLoaded", setIds);
 function setIds(){
     uberParent = document.getElementById("uberApiParent");
     uberContainer = document.getElementById("uberApiContainer");
+    uberLogo = document.getElementById("uberLogo");
 
     revvParent = document.getElementById("revvApiParent");
     revvContainer = document.getElementById("revvApiContainer");
+    revvLogo = document.getElementById("revvLogo");
 }
 
 function stretchUberApi(){    
@@ -24,7 +26,11 @@ function stretchUberApi(){
         uberParent.style.width="88px";
         uberContainer.style.width="61px";
         uberParent.style.left="212px";
-
+        uberContainer.style.display="block";
+        Array.from(document.getElementsByClassName("uberItem")).forEach(elem => {
+            elem.style.display = "none";
+        });
+        uberLogo.style.display = "block";
  
     }
     else{
@@ -32,12 +38,22 @@ function stretchUberApi(){
         uberParent.style.width="300px";
         uberContainer.style.width="272px";
         uberParent.style.left="0px";
+        uberContainer.style.display="grid";
+        Array.from(document.getElementsByClassName("uberItem")).forEach(elem => {
+            elem.style.display = "block";
+        });
+        uberLogo.style.display = "none";
 
         if(isOpenRevv){
             isOpenRevv = false
             revvParent.style.width="88px";
             revvContainer.style.width="61px";
             revvParent.style.left="272px";
+            revvContainer.style.display="block";
+            Array.from(document.getElementsByClassName("revvItem")).forEach(elem => {
+                elem.style.display = "none";
+            });
+            revvLogo.style.display = "block";
         }
     }
 }
@@ -49,11 +65,28 @@ function stretchRevvApi(){
         revvParent.style.width="88px";
         revvContainer.style.width="61px";
         revvParent.style.left="272px";
+        revvContainer.style.display="block";
+        Array.from(document.getElementsByClassName("revvItem")).forEach(elem => {
+            elem.style.display = "none";
+        });
+        revvLogo.style.display = "block";
 
         if(!isOpenUber){
             uberParent.style.width="88px";
             uberContainer.style.width="61px";
             uberParent.style.left="212px";
+            uberContainer.style.display="block";
+        }
+        if(isOpenUber){
+            isOpenUber = true
+            uberParent.style.width="300px";
+            uberContainer.style.width="272px";
+            uberParent.style.left="0px";
+            uberContainer.style.display="grid";
+            Array.from(document.getElementsByClassName("uberItem")).forEach(elem => {
+                elem.style.display = "block";
+            });
+            uberLogo.style.display = "none";
         }
     }
     else{
@@ -61,10 +94,26 @@ function stretchRevvApi(){
         revvParent.style.width="300px";
         revvContainer.style.width="272px";
         revvParent.style.left="61px";
-
+        revvContainer.style.display="grid";
+        Array.from(document.getElementsByClassName("revvItem")).forEach(elem => {
+            elem.style.display = "block";
+        });
+        revvLogo.style.display = "none";
         if(!isOpenUber){
             uberParent.style.left = "0px";
+            Array.from(document.getElementsByClassName("uberItem")).forEach(elem => {
+                elem.style.display = "none";
+            });
+            uberLogo.style.display = "block";
         }
-        
+        if(isOpenUber){   
+            console.log("hello");
+            uberContainer.style.display="block"
+            uberContainer.style.width="61px";
+            Array.from(document.getElementsByClassName("uberItem")).forEach(elem => {
+                elem.style.display = "none";
+            });
+            uberLogo.style.display = "block";
+        }
     }
 }
