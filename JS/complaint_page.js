139b4118b2825014b4e7bd4e0f56578972b4015f
khushboo-rathod <co2019.khushboo.rathod@ves.ac.in>
2022-04-30 20:10:29 +0530

    minor changes

--- JS/complaint_page.js
+++ JS/complaint_page.js
@@ -1,4 +1,3 @@
-
 function takeInput() {
     let input = document.createElement('input');
     input.type = 'file';
@@ -7,3 +6,16 @@ function takeInput() {
     input.click();
 }
 
+function submitComplaintForm(){
+    var name, email, category, explanation;
+    name = document.getElementById('name').value;
+    email = document.getElementById('email').value;
+    category = document.getElementById('category').value;
+    explanation = document.getElementById('complaint').value;
+    if(name == "" || email == "" || category == "" || explanation == ""){
+      alert('Please fill all the details')
+    }
+    else{
+      document.getElementById("contact").submit();
+    }
+}
