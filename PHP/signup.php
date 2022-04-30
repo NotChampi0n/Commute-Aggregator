075f9a04ae41ebc04abf47990773894d7cbbba7a
khushboo-rathod <co2019.khushboo.rathod@ves.ac.in>
2022-04-30 22:19:19 +0530

    backend almost done

--- PHP/signup.php
+++ PHP/signup.php
@@ -7,17 +7,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     $con=mysqli_connect("localhost","id18850211_commute_login","Khushboo(2004)","id18850211_login");
 
-    $sql="insert into login (name,email,phone_number,password) values (?,?,?,?)";
-    $stmt=$con->prepare($sql);
-    $stmt->bind_param("ssss",$name,$email,$phno,$pwd);
-    $result=$stmt->execute();
-    if($result)
-    {
-        header("Location:https://commuteagg.000webhostapp.com/HTML/Login.html");
-    }
-    else
-    {
-        header("Location:https://commuteagg.000webhostapp.com/HTML/Signup.html");
+    if($stmt = $con->prepare("select * from login where email = ?")){
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
+        if ($stmt->num_rows > 0) {
+            echo "<script>alert('You are already registered!! Please login directly')</script>";
+        } 
+        else {
+            $sql="insert into login (name,email,phone_number,password) values (?,?,?,?)";
+            $stmt=$con->prepare($sql);
+            $stmt->bind_param("ssss",$name,$email,$phno,$pwd);
+            $result=$stmt->execute();
+            if($result){
+                header("Location:https://commuteagg.000webhostapp.com/HTML/Login.html");
+            }
+            else{
+                header("Location:https://commuteagg.000webhostapp.com/HTML/Signup.html");
+            }
+        }
     }
     mysqli_close($con);
 }
