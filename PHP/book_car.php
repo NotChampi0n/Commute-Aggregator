0d5adb20f9a15dae1ece8dbed71112a4151ee68a
khushboo-rathod <co2019.khushboo.rathod@ves.ac.in>
2022-05-03 21:34:57 +0530

    Mail after book now - half done

---
+++ PHP/book_car.php
@@ -0,0 +1,98 @@
+<?php
+
+function generateEstimatedTime($length = 1)
+{
+    $characters = '0123456789';
+    $charactersLength = strlen($characters);
+    $time = '';
+    for ($i = 0; $i < $length; $i++) {
+        $time .= $characters[rand(0, $charactersLength - 1)];
+    }
+    // echo "Time:$time<br>";
+    return $time;
+}
+
+function generatePhoneNumber($length = 10)
+{
+    $characters = '0123456789';
+    $charactersLength = strlen($characters);
+    $phone = '';
+    for ($i = 0; $i < $length; $i++) {
+        $phone .= $characters[rand(0, $charactersLength - 1)];
+    }
+    // echo "Phone:$phone<br>";
+    return $phone;
+}
+
+function generateOTP($length = 4)
+{
+    $characters = '0123456789';
+    $charactersLength = strlen($characters);
+    $otp = '';
+    for ($i = 0; $i < $length; $i++) {
+        $otp .= $characters[rand(0, $charactersLength - 1)];
+    }
+    // echo "<br>Otp:$otp<br>";
+    return $otp;
+}
+
+function generateCarNumber()
+{
+    $characters = '0123456789';
+    $secondPlace = '';
+    for ($i = 0; $i < 2; $i++) {
+        $secondPlace .= $characters[rand(0, 2)];
+    }
+    $thirdPlace = '';
+    $charactersForThirdPlace = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charLen = strlen($charactersForThirdPlace);
+    for ($i = 0; $i < 2; $i++) {
+        $thirdPlace .= $charactersForThirdPlace[rand(0, $charLen-1)];
+    }
+    $fourthPlace = '';
+    $charactersForFourthPlace = '0123456789';
+    $charLength = strlen($charactersForFourthPlace);
+    for ($i = 0; $i < 4; $i++) {
+        $fourthPlace .= $charactersForFourthPlace[rand(0, $charLength - 1)];
+    }
+    // echo "<br>MH $secondPlace $thirdPlace $fourthPlace";
+    return "MH $secondPlace $thirdPlace $fourthPlace";
+
+}
+
+$carNumber = generateCarNumber();
+$otp = generateOTP();
+$phone = generatePhoneNumber();
+$time = generateEstimatedTime();
+$from = $_POST['from'];
+$to = $_POST['to'];
+
+$to_email = $_COOKIE['email'];
+$subject="Your ride is booked!";
+$body="Hello user, thank you for booking your ride with Commute Aggregator.  
+                
+Please find your ride details below:
+Pickup Location - $from
+Drop Location - $to
+Estimated cost - 
+Vehicle - 
+Vehicle Number - $carNumber
+Driver Name - 
+Drive Contact - $phone
+Arrival Time - $time Minutes
+OTP - $otp
+
+Incase if you wish to cancel the ride, please reply to this mail with CANCEL. Feel free to reach out in case of any confusion. 
+
+Have a safe ride, thank you. Regards, 
+Commute Aggregator";
+$from_email="From: co2019.khushboo.rathod@ves.ac.in";
+
+(mail($to_email, $subject, $body, $from_email));
+
+echo "<script>alert('A confirmation mail is sent to your registered email id, Thank you!')</script>";
+
+$conn = mysqli_connect("localhost","id18850211_commute_login","Khushboo(2004)","id18850211_login");
+$query = "update login set from_location='$from', to_location='$to' where email='$to_email'";
+$run = mysqli_query($conn, $query);
+?>
--- PHP/login.php
+++ PHP/login.php
@@ -1,7 +1,9 @@
 <?php
+
+$email=$_POST["email"];
+$password=$_POST["password"];
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $email=$_POST["email"];
-    $password=$_POST["password"];
 
     $con=mysqli_connect("localhost","id18850211_commute_login","Khushboo(2004)","id18850211_login");
 
@@ -11,6 +13,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $stmt->execute();
     $result=$stmt->get_result();
     if($result->num_rows==1){
+        setcookie('email', $email, time() + (86400 * 30), "/");
         header("Location:https://commuteagg.000webhostapp.com/HTML/Homepage.html");
     }
     else{
